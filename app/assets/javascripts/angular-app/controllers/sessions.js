b102beaccf0632412bf155504f98b81a4ed91e70
Drakken Saer <shadowdrakkeen@gmail.com>
2015-08-11 23:38:12 +0000

    fixed a typo in sessions.js

--- app/assets/javascripts/angular-app/controllers/sessions.js
+++ app/assets/javascripts/angular-app/controllers/sessions.js
@@ -1,10 +1,16 @@
 app.controller('SessionsCtrl', ['$scope','$location', function ($scope, $location) {
   $scope.$on('auth:login-error', function(ev, reason) {
     $scope.error = reason.errors[0];
+  })
 }]);
 
+
 app.run(['$rootScope','$location', function ($rootScope, $location) {
-  $rootScope.$on('auth:login-success', 'auth:logout-success', function() {
+  $rootScope.$on('auth:login-success', function() {
+    $location.path('/');
+  });
+
+  $rootScope.$on('auth:logout-success', function() {
     $location.path('/');
   });
 }]);
